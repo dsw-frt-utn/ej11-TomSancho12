8d6ebae3f9ac6e4e11e86e512e330fd2b3d03bf9
Tomás <tomisancho1@gmail.com>
2026-05-22 01:01:08 -0300

    Clase CasoDictionary terminada

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,10 +1,35 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
 
-//Crear un diccionario donde la clave sea el legajo y el valor el alumno
-//Incluir un método para agregar un alumno al diccionario
-//Incluir un método para buscar un alumno utilizando la clave
-//Incluir un método para retornar el diccionario
-//Incluir un método para eliminar un alumno utilizando la clave
+namespace Dsw2026Ej11.Collections;
 public class CasoDictionary
 {
+    private Dictionary<int, Alumno> alumnos = new Dictionary<int, Alumno>();
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        this.alumnos.Add(alumno.Id, alumno);
+    }
+    public Alumno BuscarAlumno(int legajo)
+    {
+        if (this.alumnos.ContainsKey(legajo))
+        {
+            return this.alumnos[legajo];
+        }
+        return null;
+    }
+    public Dictionary<int, Alumno> ObtenerAlumnos()
+    {
+        return this.alumnos;
+    }
+    public void EliminarAlumno(int legajo)
+    {
+        if (this.alumnos.ContainsKey(legajo))
+        {
+            this.alumnos.Remove(legajo);
+        }
+        else
+        {
+            throw new ArgumentException("No existe un alumno con el legajo indicado.", "legajo");
+        }
+    }
 }
