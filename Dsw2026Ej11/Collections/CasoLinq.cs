5a909ebf1dd1d6c62f17dc26fa7e2f2f0ecee9af
Tomás <tomisancho1@gmail.com>
2026-05-22 22:47:35 -0300

    Clase y ejemplos finalizados

--- Dsw2026Ej11/Collections/CasoLinq.cs
+++ Dsw2026Ej11/Collections/CasoLinq.cs
@@ -1,19 +1,48 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
 
-/*
- * Para cada punto crear un método que permita:
- * 1. Obtener el primer libro (GetPrimero)
- * 2. Obtener el último libro (GetUltimo)
- * 3. Obtener la suma de precios (GetTotalPrecios)
- * 4. Obtener el promedio de precios (GetPromedioPrecios)
- * 5. Obtener la lista de libros con Id mayor a 15 (GetListById)
- * 6. Obtener una lista de cada libro con su título y precio en formato moneda (GetLibros) (debe retornar una lista de string)
- * 7. Obtener el libro con el precio más alto (GetMayorPrecio)
- * 8. Obtener el libro con el precio más bajo (GetMenorPrecio)
- * 9. Obtener los libros cuyo precio sea mayor al promedio (GetMayorPromedio)
- * 10. Obtener los libros ordenados por título de forma descendente
- * En todos los casos debe aplicarse LINQ
- */
+namespace Dsw2026Ej11.Collections;
 public class CasoLinq
 {
+    private List<Libro> libros = Libro.CrearLista();
+    public Libro GetPrimero()
+    {
+        return this.libros.First();
+    }
+    public Libro GetUltimo()
+    {
+        return this.libros.Last();
+    }
+    public decimal GetTotalPrecios()
+    {
+        return this.libros.Sum(libro => libro.Precio);
+    }
+    public double GetPromedioPrecios()
+    {
+        return (double)this.libros.Average(libro => libro.Precio);
+    }
+    public List<Libro> GetListById()
+    {
+        return this.libros.Where(libro => libro.Id > 15).ToList();
+    }
+    public List<string> GetLibros()
+    {
+        return this.libros.Select(libro => $"{libro.Titulo} - {libro.Precio:C}").ToList();
+    }
+    public Libro GetMayorPrecio()
+    {
+        return this.libros.OrderByDescending(libro => libro.Precio).First();
+    }
+    public Libro GetMenorPrecio()
+    {
+        return this.libros.OrderBy(libro => libro.Precio).First();
+    }
+    public List<Libro> GetMayorPromedio()
+    {
+        decimal promedio = this.libros.Average(libro => libro.Precio);
+        return this.libros.Where(libro => libro.Precio > promedio).ToList();
+    }
+    public List<Libro> GetOrdenadosPorTitulo()
+    {
+        return this.libros.OrderByDescending(libro => libro.Titulo).ToList();
+    }
 }
